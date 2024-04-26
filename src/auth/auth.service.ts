4cea5925504d6dad5c45b5adc8bce64d7ed675c7
Ryan Alves <ryan.alves@gestaokaizen.com.br>
2024-04-26 15:24:01 -0300

    feat-2FA

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,7 +5,10 @@ import { User } from 'src/users/user.entity';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { ArtistsService } from 'src/artists/artists.service';
-import { Payload } from './types';
+import { Enable2FAType, Payload } from './types';
+
+import * as speakeasy from 'speakeasy';
+import { UpdateResult } from 'typeorm';
 
 @Injectable()
 export class AuthService {
@@ -15,23 +18,74 @@ export class AuthService {
     private artistsService: ArtistsService,
   ) {}
 
-  async login(loginDTO: LoginDTO): Promise<{ accessToken: string }> {
-    const user = await this.userService.findOne(loginDTO);
+  async login(
+    loginDTO: LoginDTO,
+  ): Promise<{ accessToken: string } | { validate2FA: string; message: string }> {
+    const user = await this.userService.findOne(loginDTO); // 1.
 
     const passwordMatched = await bcrypt.compare(loginDTO.password, user.password);
 
     if (passwordMatched) {
       delete user.password;
       const payload: Payload = { email: user.email, userId: user.id };
-      const artist = await this.artistsService.findArtist(user.id);
+      const artist = await this.artistsService.findArtist(user.id); // 2
       if (artist) {
         payload.artistId = artist.id;
+      }
+      if (user.enable2FA && user.twoFASecret) {
+        //1.
+        // sends the validateToken request link
+        // else otherwise sends the json web token in the response
+        return {
+          //2.
+          validate2FA: 'http://localhost:3000/auth/validate-2fa',
+          message: 'Please sends the one time password/token from your Google Authenticator App',
+        };
       }
       return {
         accessToken: this.jwtService.sign(payload),
       };
     } else {
-      throw new UnauthorizedException('Password does not match');
+      throw new UnauthorizedException('Password does not match'); // 5.
     }
+  }
+  async enable2FA(userId: number): Promise<Enable2FAType> {
+    const user = await this.userService.findById(userId);
+    if (user.enable2FA) {
+      return { secret: user.twoFASecret };
+    }
+    const secret = speakeasy.generateSecret();
+    console.log(secret);
+    user.twoFASecret = secret.base32;
+    await this.userService.updateSecretKey(user.id, user.twoFASecret);
+    return { secret: user.twoFASecret };
+  }
+
+  async validate2FAToken(userId: number, token: string): Promise<{ verified: boolean }> {
+    try {
+      // find the user on the based on id
+      const user = await this.userService.findById(userId);
+
+      // extract his 2FA secret
+
+      // verify the secret with token by calling the speakeasy verify method
+      const verified = speakeasy.totp.verify({
+        secret: user.twoFASecret,
+        token: token,
+        encoding: 'base32',
+      });
+
+      // if validated then sends the json web token in the response
+      if (verified) {
+        return { verified: true };
+      } else {
+        return { verified: false };
+      }
+    } catch (err) {
+      throw new UnauthorizedException('Error verifying token');
+    }
+  }
+  async disable2FA(userId: number): Promise<UpdateResult> {
+    return this.userService.disable2FA(userId);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { CreateUserDTO } from './dto/create-user-dto';
 import * as bcrypt from 'bcryptjs';
 import { LoginDTO } from 'src/auth/dto/login.dto';
@@ -10,22 +10,43 @@ import { LoginDTO } from 'src/auth/dto/login.dto';
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    private userRepository: Repository<User>, // 1.
   ) {}
 
   async create(userDTO: CreateUserDTO): Promise<User> {
-    const salt = await bcrypt.genSalt();
-    userDTO.password = await bcrypt.hash(userDTO.password, salt);
-    const user = await this.usersRepository.save(userDTO);
-    delete user.password;
-    return user;
+    const salt = await bcrypt.genSalt(); // 2.
+    userDTO.password = await bcrypt.hash(userDTO.password, salt); // 3.
+    const user = await this.userRepository.save(userDTO); // 4.
+    delete user.password; // 5.
+    return user; // 6.
   }
 
   async findOne(data: LoginDTO): Promise<User> {
-    const user = await this.usersRepository.findOneBy({ email: data.email });
+    const user = await this.userRepository.findOneBy({ email: data.email });
     if (!user) {
       throw new UnauthorizedException('Could not find user');
     }
     return user;
+  }
+  async findById(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id: id });
+  }
+  async updateSecretKey(userId, secret: string): Promise<UpdateResult> {
+    return this.userRepository.update(
+      { id: userId },
+      {
+        twoFASecret: secret,
+        enable2FA: true,
+      },
+    );
+  }
+  async disable2FA(userId: number): Promise<UpdateResult> {
+    return this.userRepository.update(
+      { id: userId },
+      {
+        enable2FA: false,
+        twoFASecret: null,
+      },
+    );
   }
 }
